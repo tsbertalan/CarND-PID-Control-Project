239a5d6b39b6cd4cb854da9bf4320c38a8a6696c
Tom Bertalan <tom@tombertalan.com>
2018-04-18 01:18:44 -0400

    Make a first draft of twiddle tuning.

--- src/twiddle.cpp
+++ src/twiddle.cpp
@@ -1,127 +1,182 @@
-#include <uWS/uWS.h>
+#include "twiddle.h"
 #include <iostream>
-#include "json.hpp"
-#include "PID.h"
-#include <math.h>
-#include <algorithm>  // std::min, std::max
 
-#define TARGETSPEED 20.0
-#define CREEPSPEED 3.0
-#define MAXANGLE 25.0
+using namespace std;
 
-// for convenience
-using json = nlohmann::json;
+constexpr double DEFAULT_DIFF_PARAMS = 0.1;
 
-// For converting back and forth between radians and degrees.
-constexpr double pi() { return M_PI; }
 
-double deg2rad(double x) { return x * pi() / 180; }
-
-double rad2deg(double x) { return x * 180 / pi(); }
-
-// Checks if the SocketIO event has JSON data.
-// If there is data the JSON object in string format will be returned,
-// else the empty string "" will be returned.
-std::string hasData(std::string s) {
-    auto found_null = s.find("null");
-    auto b1 = s.find_first_of("[");
-    auto b2 = s.find_last_of("]");
-    if (found_null != std::string::npos) {
-        return "";
-    } else if (b1 != std::string::npos && b2 != std::string::npos) {
-        return s.substr(b1, b2 - b1 + 1);
-    }
-    return "";
+Twiddler::Twiddler(int nparams=6, double tol=0.001) {
+	vector<double> parameters(nparams, 0);
+	vector<double> diff_parameters(nparams, DEFAULT_DIFF_PARAMS);
+	i_param = -1;
+	iterations = 0;
+	this->tol = tol;
+	best_error = std::numeric_limits<double>::infinity();
+	last_change = NONE;
 }
 
-int main() {
-    uWS::Hub h;
 
-    PID pid_steering;
-    PID pid_throttle;
-    // TODO: Need to tune these.
-    pid_steering.Init(0.1, 0.0003, 3);
-    pid_throttle.Init(0.3, 0, 0.02);
+bool Twiddler::twiddle(double error) {
 
-    h.onMessage([&pid_steering, &pid_throttle](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
-        // "42" at the start of the message means there's a websocket message event.
-        // The 4 signifies a websocket message
-        // The 2 signifies a websocket event
-        if (length && length > 2 && data[0] == '4' && data[1] == '2') {
-            auto s = hasData(std::string(data).substr(0, length));
-            if (s != "") {
-                auto j = json::parse(s);
-                std::string event = j[0].get<std::string>();
-                if (event == "telemetry") {
-                    // j[1] is the data JSON object
-                    double cte = std::stod(j[1]["cte"].get<std::string>());
-                    double speed = std::stod(j[1]["speed"].get<std::string>());
-                    double angle = std::stod(j[1]["steering_angle"].get<std::string>());
+	cout << "Twiddling." << endl;
+	cout << "Best error is " << best_error << "." << endl;
 
-                    std::cout << "Current angle/speed is " << angle << "/" << speed << std::endl;
+	// Save the error history.
+	errors.push_back(error);
 
-                    pid_steering.UpdateError(cte);
+	// Always increment the parameter index.
+	i_param++;
 
-                    double speedTarget = TARGETSPEED * (1 - fabs(angle/MAXANGLE) - .5*fabs(cte));
-                    std::cout << "Speed target is " << speedTarget;
-                    speedTarget = std::max(CREEPSPEED, speedTarget);
-                    std::cout << " --> " << speedTarget << std::endl;
-                    pid_throttle.UpdateError(speed - speedTarget);
+	// If we've finished the last parameter loop, check for convergence.
+	if(i_param == parameters.size()) {
+		cout << "Finished parameter loop!" << endl;
 
-                    /*
-                    * TODO: Calcuate steering value here, remember the steering value is
-                    * [-1, 1].
-                    * NOTE: Feel free to play around with the throttle and speed. Maybe use
-                    * another PID controller to control the speed!
-                    */
-                    double steer_value = std::max(-1.0, std::min(1.0, pid_steering.TotalError()));
-                    double throttle = pid_throttle.TotalError();
+		// Increment the count-of-loops.
+		iterations++;
 
-                    // DEBUG
-                    std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
+		// Sum the increment vector.
+		double sdp = 0;
+	    for(auto& d : diff_parameters) {
+	    	sdp += d;
+	    }
 
-                    json msgJson;
-                    msgJson["steering_angle"] = steer_value;
-                    msgJson["throttle"] = throttle;
-                    auto msg = "42[\"steer\"," + msgJson.dump() + "]";
-                    std::cout << msg << std::endl;
-                    ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
-                }
-            } else {
-                // Manual driving
-                std::string msg = "42[\"manual\",{}]";
-                ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
-            }
-        }
-    });
+	    // Check for convergence.
+	    if(sdp <= tol) {
+	    	// If we've converged, just say so.
+	    	return true;
+	    } else {
+	    	// If we haven't converged, reset the counter for the next loop.
+			i_param = 0;
+	    }
+	}
 
-    // We don't need this since we're not using HTTP but if it's removed the program
-    // doesn't compile :-(
-    h.onHttpRequest([](uWS::HttpResponse *res, uWS::HttpRequest req, char *data, size_t, size_t) {
-        const std::string s = "<h1>Hello world!</h1>";
-        if (req.getUrl().valueLength == 1) {
-            res->end(s.data(), s.length());
-        } else {
-            // i guess this should be done more gracefully?
-            res->end(nullptr, 0);
-        }
-    });
+	// If we haven't converged, or we're in the middle of a loop,
+	// we now do the actual twiddle.
 
-    h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
-        std::cout << "Connected!!!" << std::endl;
-    });
+	// Depending on what we did last time an error was passed, 
+	// we can now do one of three things.
 
-    h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code, char *message, size_t length) {
-        ws.close();
-        std::cout << "Disconnected" << std::endl;
-    });
+	last_change_t new_change;
 
-    int port = 4567;
-    if (h.listen(port)) {
-        std::cout << "Listening to port " << port << std::endl;
-    } else {
-        std::cerr << "Failed to listen to port" << std::endl;
-        return -1;
-    }
-    h.run();
+	// We haven't tried this dp yet.
+	// Try an increase.
+	if(last_change == NONE) {
+		cout << "Try an increase." << endl;
+		parameters[i_param] += diff_parameters[i_param];
+		new_change = INCREASE;
+
+	// We did an increase last time; how did it work out?
+	} else if(last_change == INCREASE) {
+
+		// If it succeeded, accelerate.
+		if(error < best_error) {
+			cout << "Increase succeeded!" << endl;
+			best_error = error;
+			diff_parameters[i_param] *= 1.1;
+
+			// Move on.
+			i_param++;
+			new_change = NONE;
+
+		// If it failed, try a decrease.
+		} else {
+			cout << "Try a decrease." << endl;
+			parameters[i_param] -= 2 * diff_parameters[i_param];
+			new_change = DECREASE;
+		}
+
+	// We did a decrease last time; how did it work out?
+	} else if(last_change == DECREASE) {
+		
+		// If it succeeded, accelerate.
+		if(error < best_error) {
+			cout << "Decrease succeeded!" << endl;
+			best_error = error;
+			diff_parameters[i_param] *= 1.1;
+
+		} else {
+			// If the decrease also failed, restore the original parameter value and decelerate.
+			cout << "Decrease failed!" << endl;
+			parameters[i_param] += diff_parameters[i_param];
+			diff_parameters[i_param] *= 0.9;
+		}
+
+		// Regardless, we're moving on.
+		i_param++;
+		new_change = NONE;
+	}
+
+	last_change = new_change;
+	return false;
+}
+
+
+std::vector<double> Twiddler::get_params() {
+	return parameters;
+}
+
+void Twiddler::set_params(vector<double> new_parameters) {
+	parameters = new_parameters;
+	diff_parameters.clear();
+	for(auto & unused_p : parameters) {
+		diff_parameters.push_back(DEFAULT_DIFF_PARAMS);
+	}
+}
+
+
+TwiddlerManager::TwiddlerManager(std::vector<PID*>& pids, int tmax) {
+	this->pids = pids;
+	// for(auto &pid : pids) {
+	// 	this->pids.push_back(pid);
+	// }
+	this->tmax = tmax;
+
+	int nparams = pids.size() * 3;
+
+	twiddler = Twiddler(nparams);
+
+	// Extract the existing parameters.
+	vector<double> new_parameters(nparams);
+	int i = 0;
+	for(auto &pid : pids) {
+		new_parameters[i*3+0] = pid->Kp;
+		new_parameters[i*3+1] = pid->Ki;
+		new_parameters[i*3+2] = pid->Kd;
+		i++;
+	}
+	twiddler.set_params(new_parameters);
+
+	errors_seen = 0;
+	integrated_error = 0;
+}
+
+void TwiddlerManager::process_error(double error) {
+
+	// Integrate the error.
+	integrated_error += error;
+	errors_seen++;
+
+	// If we've added up enough errors, take a mean.
+	if(errors_seen >= tmax) {
+
+		integrated_error /= errors_seen;
+
+		cout << "Mean error on run: " << integrated_error << endl;
+
+		// Then twiddle the parameters.
+		twiddler.twiddle(integrated_error);
+
+		// Apply the new parameters to the PIDs.
+		int i = 0;
+		vector<double> p = twiddler.get_params();
+		for(auto &pid : pids) {
+			pid->Init(p[i*3], p[i*3+1], p[i*3+2]);
+			i++;
+		}
+
+		// Reset the count.
+		integrated_error = 0;
+		errors_seen = 0;
+	}
 }
---
+++ src/twiddle.h
@@ -0,0 +1,49 @@
+#ifndef TWIDDLE_H
+#define TWIDDLE_H
+
+#include <vector>
+#include <limits>
+#include "PID.h"
+
+enum last_change_enum { INCREASE, DECREASE, NONE };
+typedef enum last_change_enum last_change_t;
+
+
+class Twiddler {
+
+private:
+  std::vector<double> parameters;
+  std::vector<double> diff_parameters;
+  std::vector<double> errors;
+
+  unsigned int i_param;
+  int iterations;
+  double tol;
+  double best_error;
+  last_change_t last_change;
+
+public:
+  Twiddler(int nparams, double tol);
+  bool twiddle(double error);
+  std::vector<double> get_params();
+  void set_params(std::vector<double> new_parameters);
+};
+
+class TwiddlerManager {
+
+private:
+  std::vector<PID*> pids;
+  Twiddler twiddler;
+  
+  double integrated_error;
+  int errors_seen;
+  int tmax;
+
+public:
+  TwiddlerManager(std::vector<PID*>& pids, int tmax);
+  void process_error(double error);
+
+};
+
+
+#endif /* TWIDDLE_H */
